dbe4a40d08106ecca9d6e557232d609c0e46e546
scott dallamura <scott.dallamura@reachmg.com>
2014-08-30 14:24:14 -0400

    added missing SignalR.Protocols.ts

---
+++ NodeR/NodeR/SignalR.Protocol.ts
@@ -0,0 +1,50 @@
+﻿import SignalRInterfaces = require("./SignalR.Interfaces");
+
+/**
+ * Expands a response to a server method.
+ * @param minified The minified response
+ */
+export function expandServerHubResponse(minified: SignalRInterfaces.MinifiedServerHubResponse): SignalRInterfaces.ServerHubResponse {
+	return {
+		State: minified.S,
+		Result: minified.R,
+		Progress: !!minified.P ? {
+			Id: minified.P.I,
+			Data: minified.P.D
+		} : null,
+		Id: minified.I,
+		IsHubException: minified.H,
+		Error: minified.E,
+		StackTrace: minified.T,
+		ErrorData: minified.D
+	};
+}
+
+/**
+ * Expands a client hub invocation.
+ * @param minified The minified invocation
+ */
+export function expandClientHubInvocation(minified: SignalRInterfaces.MinifiedClientHubInvocation): SignalRInterfaces.ClientHubInvocation {
+	return {
+		Hub: minified.H,
+		Method: minified.M,
+		Args: minified.A,
+		State: minified.S
+	};
+}
+
+/**
+ * Expands a message from the server.
+ * @param minified The minified message
+ */
+export function expandSignalRMessage(minified: SignalRInterfaces.MinifiedSignalRMessage): SignalRInterfaces.SignalRMessage {
+	return {
+		MessageId: minified.C,
+		Messages: minified.M,
+		Initialized: !!minified.S,
+		Disconnect: !!minified.D,
+		ShouldReconnect: !!minified.T,
+		LongPollDelay: minified.L,
+		GroupsToken: minified.G
+	};
+}
